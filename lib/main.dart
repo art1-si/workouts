43103bca69e1a31fcb429e34af4e254d5b3bb26b
artur-touchwonders <artur@touchwonders.com>
2023-11-04 20:02:51 +0100

    added: firebase configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:workouts/configuration/firebase/firebase_options.dart';
 import 'package:workouts/workouts_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const WorkoutsApp(),
   );

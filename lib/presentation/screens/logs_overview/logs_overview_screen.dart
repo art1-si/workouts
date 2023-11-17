85117ecc451f5cf351e77b72b67d8ad3057653bd
“a.sikora” <artur@touchwonders.com>
2023-11-17 19:22:38 +0100

    added: header

--- lib/presentation/screens/logs_overview/logs_overview_screen.dart
+++ lib/presentation/screens/logs_overview/logs_overview_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:workouts/presentation/screens/logs_overview/widgets/date_selector.dart';
 import 'package:workouts/presentation/screens/logs_overview/widgets/logs_list_view.dart';
+import 'package:workouts/presentation/theme/typography.dart';
 import 'package:workouts/presentation/widgets/calendar/calendar_controller.dart';
 import 'package:workouts/presentation/widgets/calendar/infinite_scroll_calendar.dart';
 
@@ -20,7 +21,15 @@ class _LogsOverviewScreenState extends ConsumerState<LogsOverviewScreen> {
     return Scaffold(
       body: Column(
         children: [
-          const SizedBox(height: 70),
+          const SizedBox(height: 64),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: StyledText.headlineMedium('Workouts'),
+            ),
+          ),
+          const SizedBox(height: 24),
           DateSelector(datePickerController: _datePickerController),
           const Expanded(child: LogsListView()),
         ],

2068dcff0a29439d5fdce453029eba7128d244de
“a.sikora” <artur@touchwonders.com>
2023-11-17 20:09:49 +0100

    changed: removed code cleanup

--- lib/application/workout_logs/workout_logs_for_exercise_notifier.dart
+++ lib/application/workout_logs/workout_logs_for_exercise_notifier.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:workouts/application/workout_logs/models/workout_log_view_model.dart';
 import 'package:workouts/application/workout_logs/workout_logs_controller.dart';

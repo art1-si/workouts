c4c1375df3eaeca0619e619071c9d1428a6e3de5
a.sikora <artsikora1@gmail.com>
2024-10-12 18:05:09 +0200

    added: Implemented getExercises for ExerciseLocalDataSource

--- lib/data/sources/exercise_data_source/exercise_local_data_source.dart
+++ lib/data/sources/exercise_data_source/exercise_local_data_source.dart
@@ -3,9 +3,18 @@ import 'package:workouts/data/sources/exercise_data_source/exercise_data_source.
 import 'package:workouts/data/sources/local_data_source.dart';
 
 final class ExerciseLocalDataSource extends LocalDataSource implements ExerciseDataSource {
+  const ExerciseLocalDataSource({
+    required super.sqlDatabaseClient,
+  });
+
+  static const _table = 'exercise';
+
   @override
-  Future<List<ExerciseDto>> getExercises() {
-    // TODO: implement getExercises
-    throw UnimplementedError();
+  Future<List<ExerciseDto>> getExercises() async {
+    final exercises = await sqlDatabaseClient.query(
+      table: _table,
+      parser: ExerciseDto.fromMap,
+    );
+    return exercises;
   }
 }
--- lib/data/sources/local_data_source.dart
+++ lib/data/sources/local_data_source.dart
@@ -1,3 +1,9 @@
+import 'package:workouts/data/clients/sql_client/sql_database_client.dart';
+
 abstract base class LocalDataSource {
-  const LocalDataSource();
+  const LocalDataSource({
+    required this.sqlDatabaseClient,
+  });
+
+  final SqlDatabaseClient sqlDatabaseClient;
 }
